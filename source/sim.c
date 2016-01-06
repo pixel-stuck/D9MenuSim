c53dda995ca91a7168e50b3408463f27180b560c
dark-samus <mathmcclintic@gmail.com>
2016-01-05 22:49:53 -0500

    displays images on the bottom screen and shows the image it's currently (trying) to access

--- source/sim.c
+++ source/sim.c
@@ -1,8 +1,28 @@
 #include <stdio.h>
-#include <stdint.h>
+#include <stdlib.h>
 #include <string.h>
 
 #include <3ds.h>
+
+u8* buffer;
+char n[19];
+
+void displayimage()
+{
+	FILE *file = fopen(n,"rb");
+	if (file == NULL) return 0;
+	fseek(file,0,SEEK_END);
+	off_t size = ftell(file);
+	fseek(file,0,SEEK_SET);
+	buffer=malloc(size);
+	if(!buffer) return 0;
+	off_t bytesRead = fread(buffer,1,size,file);
+	fclose(file);
+	if(size!=bytesRead) return 0;
+	memcpy(gfxGetFramebuffer(GFX_BOTTOM, GFX_BOTTOM, NULL, NULL), buffer, size);
+	gfxSwapBuffersGpu();
+	gspWaitForEvent(GSPGPU_EVENT_VBlank0, false);
+}
 
 int main()
 {
@@ -20,7 +40,9 @@ int main()
 		}
 	}
 	m = 0;
-	printf("currently displaying menu%04i.bin\n", l[m]);
+	snprintf(n, sizeof(n), "/D9UI/menu%04i.bin", l[m]);
+	printf("%s\n", n);
+	displayimage();
 	while(l[m] >= 0 && l[m] <= 1208 ){
 		hidScanInput();
 		if(hidKeysDown() & KEY_START) break;
@@ -31,17 +53,19 @@ int main()
 					m = 87;
 				}
 			}
-			printf("currently displaying menu%04i.bin\n", l[m]);
+			snprintf(n, sizeof(n), "/D9UI/menu%04i.bin", l[m]);
+			printf("%s\n", n);
+			displayimage();
 		}else if(hidKeysDown() & KEY_UP){
-			if (l[m - 1] < (l[m] - 1)){
-			printf("nope\n");
-			}else{
+			if (!(l[m - 1] < (l[m] - 1))){
 				--m;
 				if(m <= 0){
 					m = 0;
 				}	
 			}
-			printf("currently displaying menu%04i.bin\n", l[m]);
+			snprintf(n, sizeof(n), "/D9UI/menu%04i.bin", l[m]);
+			printf("%s\n", n);
+			displayimage();
 		}else if(hidKeysDown() & KEY_L){
 			i = ((l[m] / 100 - 1) * 100);
 			if((i < 0)){
@@ -53,7 +77,9 @@ int main()
 					m = 87;
 				}
 			}
-			printf("menu decremented\ncurrently displaying menu%04i.bin\n", l[m]);
+			snprintf(n, sizeof(n), "/D9UI/menu%04i.bin", l[m]);
+			printf("%s\n", n);
+			displayimage();
 		}else if(hidKeysDown() & KEY_R){
 			i = ((l[m] / 100 + 1) * 100);
 			if((i > 1200)){
@@ -65,7 +91,9 @@ int main()
 					m = 0;
 				}
 			}
-			printf("menu incremented\ncurrently displaying menu%04i.bin\n", l[m]);		
+			snprintf(n, sizeof(n), "/D9UI/menu%04i.bin", l[m]);
+			printf("%s\n", n);
+			displayimage();		
 		}
 	}
 	return 0;
